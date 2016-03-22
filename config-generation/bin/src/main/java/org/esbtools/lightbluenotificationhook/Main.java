7b7c8ada8748c699465c79ac8696e801cd60c880
Alec Henninger <ahenning@redhat.com>
2016-03-22 18:00:19 -0400

    Add logging and parameterize output directory

--- config-generation/bin/src/main/java/org/esbtools/lightbluenotificationhook/Main.java
+++ config-generation/bin/src/main/java/org/esbtools/lightbluenotificationhook/Main.java
@@ -9,7 +9,10 @@ import com.redhat.lightblue.metadata.parser.Extensions;
 import com.redhat.lightblue.metadata.parser.JSONMetadataParser;
 import com.redhat.lightblue.metadata.types.DefaultTypes;
 import com.redhat.lightblue.query.Projection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -22,19 +25,20 @@ public class Main {
     private final JSONMetadataParser parser;
     private final ObjectWriter jsonWriter;
     private final JsonNodeFactory jsonNodeFactory;
-    private final EntityNotificationHookConfigurationReader generator;
+    private final EntityNotificationHookConfigurationReader hookConfigReader;
 
     private static final String JSON_CONFIG_FILE_SUFFIX = "NotificationHookConfiguration.json";
     private static final Charset UTF_8 = Charset.forName("UTF-8");
 
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
     private static final ObjectWriter prettyWriter = new ObjectMapper().writerWithDefaultPrettyPrinter();
 
     public Main(JSONMetadataParser parser, ObjectWriter jsonWriter, JsonNodeFactory jsonNodeFactory,
-            EntityNotificationHookConfigurationReader generator) {
+            EntityNotificationHookConfigurationReader hookConfigReader) {
         this.parser = parser;
         this.jsonWriter = jsonWriter;
         this.jsonNodeFactory = jsonNodeFactory;
-        this.generator = generator;
+        this.hookConfigReader = hookConfigReader;
     }
 
     public static Main usingLightblueDefaults() {
@@ -50,10 +54,16 @@ public class Main {
     public void writeHookConfigurationForClass(String classFqn, ClassLoader classLoader,
             Path outputPath) throws ClassNotFoundException, IllegalAccessException,
             IOException {
+        log.info("Looking up hook configuration for class: {}", classFqn);
+
         Class notificationClass = Class.forName(classFqn, true, classLoader);
 
+        log.info("Found class: {}", classFqn);
+
         EntityNotificationHookConfiguration entityConfig =
-                generator.readConfiguration(notificationClass);
+                hookConfigReader.readConfiguration(notificationClass);
+
+        log.info("Got configuration for entity: {}", entityConfig.entityName());
 
         Projection watch = Projection.fromJson(entityConfig.watchProjection().toJson());
         Projection include = Projection.fromJson(entityConfig.includeProjection().toJson());
@@ -69,26 +79,37 @@ public class Main {
                 ? outputPath.resolve(entityConfig.entityName() + JSON_CONFIG_FILE_SUFFIX)
                 : outputPath;
 
+        log.info("Writing json configuration to: {}", jsonResultPath.toAbsolutePath());
+
         jsonWriter.writeValue(Files.newBufferedWriter(jsonResultPath, UTF_8), configAsJson);
+
+        log.info("Wrote json configuration: {}", jsonResultPath);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         List<String> helpArgs = Arrays.asList("-h", "h", "?", "-?", "help", "-help", "--help");
 
-        if (args.length < 2 || helpArgs.contains(args[0].toLowerCase())) {
+        if (args.length < 2) {
+            print("Not enough arguments provided, see usage:");
+            printUsageAndExit();
+        } else if (helpArgs.contains(args[0].toLowerCase())) {
             printUsageAndExit();
         }
 
         Main main = Main.usingLightblueDefaults();
         ClassLoader mainClassLoader = Main.class.getClassLoader();
+        Path resultDir = Paths.get(args[0]);
+
+        if (!Files.isDirectory(resultDir)) {
+            throw new FileNotFoundException("Provided result directory does not exist.");
+        }
 
         try {
+
             for (int i = 1; i < args.length; i++) {
                 String className = args[i];
 
-                // TODO(ahenning): Make output path configurable
-                main.writeHookConfigurationForClass(
-                        className, mainClassLoader, Paths.get("target/generated-sources/"));
+                main.writeHookConfigurationForClass(className, mainClassLoader, resultDir);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -97,7 +118,7 @@ public class Main {
     }
 
     private static void printUsageAndExit() {
-        print("Usage: generator com.mycompany.Notification1 com.mycompany.Notification2 ...");
+        print("Usage: generator path/to/result/dir/ com.mycompany.Notification1 com.mycompany.Notification2 ...");
         print("Expects notifications to have a static field which is an instance of ");
         print(EntityNotificationHookConfiguration.class + ". This will be output as config.");
         print("");
